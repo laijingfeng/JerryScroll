5eed42a48b30b4bf930f9ee4b690cf34cdfa2af1
laijingfeng <337292616@qq.com>
2017-06-24 09:55:19 +0800

    update

---
+++ Assets/InfinitelyLinearGridLayoutGroup.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfinitelyLinearGridLayoutGroup : MonoBehaviour
+{
+    public Dir dir = Dir.Horizontal;
+    public Vector2 cellSize = new Vector2(100f, 100f);
+    public float spacing = 10f;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public enum Dir
+    {
+        Horizontal = 0,
+        Vertical,
+    }
+}
